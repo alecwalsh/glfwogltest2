e905b53e482cf948def9f6d05067fc966195c5ee
Alec Walsh <alecwalsh1996@gmail.com>
2017-01-11 02:53:55 -0500

    Cleaned up light object shader

--- glfwogltest2/shaders/frag_light.glsl
+++ glfwogltest2/shaders/frag_light.glsl
@@ -8,35 +8,6 @@ in vec2 Normaltexcoord;
 
 out vec4 outColor;
 
-uniform sampler2D texDiffuseMap; //diffuse map
-uniform sampler2D texPuppy;
-uniform sampler2D texNormalMap;
-uniform sampler2D texSpecMap;
-uniform float time;
-uniform int numLights;
-
-uniform vec3 cameraPos;
-
-struct Material {
-	//TODO: move diffuse texture into struct
-	//sampler2D diffuse;
-    vec3 diffuse;
-    vec3 specular;
-    float shininess;
-}; 
-  
-uniform Material material;
-
-struct Light {
-    vec3 position;
-  
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-};
-
-uniform Light lights[10];  
-
 void main() {
 	outColor = vec4(1.0f);
 }
--- glfwogltest2/shaders/vert_light.glsl
+++ glfwogltest2/shaders/vert_light.glsl
@@ -1,15 +1,15 @@
 #version 150 core
 
+
+//TODO: remove unnecessary attribs after moving lightsObject into separate class
 in vec3 position;
 in vec3 color;
 in vec3 normal;
-//in vec2 texcoord;
 in vec2 normaltexcoord;
 
 out vec3 Color;
 out vec3 Normal;
 out vec3 FragPos;
-//out vec2 Texcoord;
 out vec2 Normaltexcoord;
 
 uniform mat4 model;
@@ -22,8 +22,7 @@ void main() {
 	//Normal matrix
 	Normal = mat3(transpose(inverse(model))) * normal; //This is inefficient, should calculate on CPU
 
-	FragPos = vec3(model * vec4(position, 1.0f));
-	//Texcoord = texcoord;
+	FragPos = vec3(1.0);
 	Normaltexcoord = normaltexcoord;
 	gl_Position = proj * view * model * vec4(position, 1.0);
 }
