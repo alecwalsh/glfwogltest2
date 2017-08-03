7afc32f8a1229fcea2d272ffd802cc0b1e59fdfd
Alec Walsh <alecwalsh1996@gmail.com>
2017-08-03 08:00:36 -0400

    Add error checking to shader compiling and linking

--- ShaderProgram.cpp
+++ ShaderProgram.cpp
@@ -37,16 +37,19 @@ GLuint ShaderProgram::ShaderProgramFromFiles(const char* vertShaderFile, const c
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
 	glCompileShader(vertexShader);
+    getCompileErrors(vertexShader);
 
 	// Create and compile the fragment shader
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
 	glCompileShader(fragmentShader);
+    getCompileErrors(fragmentShader);
 
 	// Link the vertex and fragment shader into a shader program
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
+    getLinkErrors(shaderProgram);
 
     //Don't need these anymore
     glDeleteShader(fragmentShader);
@@ -55,6 +58,44 @@ GLuint ShaderProgram::ShaderProgramFromFiles(const char* vertShaderFile, const c
     this->shaderProgram = shaderProgram;
 
 	return shaderProgram;
+}
+
+void ShaderProgram::getCompileErrors(GLuint shader)
+{
+    GLint compiled;
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+    if(compiled == GL_FALSE)
+    {
+        int logLength;
+        
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
+        GLchar* infoLog = new GLchar[logLength];
+        glGetShaderInfoLog(shader, logLength, &logLength, infoLog);
+        
+        printf("Compile error: %s\n", infoLog);
+        delete[] infoLog;
+        exit(EXIT_FAILURE);
+    }
+}
+
+void ShaderProgram::getLinkErrors(GLuint shaderProgram)
+{
+    GLint linked;
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &linked);
+    if(linked == GL_FALSE)
+    {
+        int logLength;
+        
+        glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &logLength);
+        GLchar* infoLog = new GLchar[logLength];
+        glGetProgramInfoLog(shaderProgram, logLength, &logLength, infoLog);
+        
+        printf("Link error: %s\n", infoLog);
+        delete[] infoLog;
+        exit(EXIT_FAILURE);
+    }
 }
 
 ShaderProgram::~ShaderProgram() {
--- ShaderProgram.h
+++ ShaderProgram.h
@@ -5,6 +5,8 @@
 class ShaderProgram
 {
 	GLuint ShaderProgramFromFiles(const char* vertShaderFile, const char* fragShaderFile);
+    void getCompileErrors(GLuint shader);
+    void getLinkErrors(GLuint shaderProgram);
 public:
 	GLuint shaderProgram;
 	ShaderProgram(char const* vertShader, char const* fragShader);
