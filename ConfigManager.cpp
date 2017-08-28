96c743ca26c40a3f5d82cf52ca0ce8d5feee47d7
Alec Walsh <alecwalsh1996@gmail.com>
2017-08-28 02:15:35 -0400

    Detect incorrect types in lua config, remove exceptions
    No longer uses exceptions to report errors in lua config files

--- ConfigManager.cpp
+++ ConfigManager.cpp
@@ -21,45 +21,35 @@ ConfigManager::~ConfigManager() {
 
 int ConfigManager::getWidth() {
     lua_getglobal(L, "width");
-    //TODO: Use lua_isinteger to detect type errors
+    if(!lua_isinteger(L, 1)) {
+        std::cout << "Error: Width must be an integer, using default value" << std::endl;
+        return width;
+    }
     auto width = lua_tointeger(L, 1);
     lua_pop(L, 1);
-    if(width == 0) {
-        throw std::runtime_error("Invalid width");
+    if(width < 1) {
+        std::cout << "Error: Invalid width, using default value" << std::endl;
+        return this->width;
     }
     return width;
 }
 
 int ConfigManager::getHeight() {
     lua_getglobal(L, "height");
-    //TODO: Use lua_isinteger to detect type errors
+    if(!lua_isinteger(L, 1)) {
+        std::cout << "Error: Height must be an integer, using default value" << std::endl;
+        return height;
+    }
     auto height = lua_tointeger(L, 1);
     lua_pop(L, 1);
-    if(height == 0) {
-        throw std::runtime_error("Invalid height");
+    if(height < 1) {
+        std::cout << "Error: Invalid height, using default value" << std::endl;
+        return this->height;
     }
     return height;
 }
 
 void ConfigManager::LoadVars() {
-    bool validWidth = true, validHeight = true;
-    try {
-        width = getWidth();
-    } catch(std::runtime_error& e) {
-        validWidth = false;
-    }
-    try {
-        height = getHeight();
-    } catch(std::runtime_error& e) {
-        validHeight = false;
-    }
-    
-    //Construct appropriate error message
-    if(!validWidth && !validHeight) {
-        std::cout << "Error: Invalid width and height, using default values" << std::endl;
-    } else if(!validWidth) {
-        std::cout << "Error: Invalid width, using default value" << std::endl;
-    } else if(!validHeight) {
-        std::cout << "Error: Invalid height, using default value" << std::endl;
-    }
+    width = getWidth();
+    height = getHeight();
 }
