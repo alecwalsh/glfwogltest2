bf4d16ac77220d83cec56e871588d67d3328ca0d
Alec Walsh <alecwalsh1996@gmail.com>
2017-11-05 12:35:13 -0500

    Made Light constructor protected
    A Light should only be created through on of its subclasses

--- include/Light.h
+++ include/Light.h
@@ -13,6 +13,7 @@ public:
     glm::vec3 diffuse;
     glm::vec3 specular;
     
-    Light(glm::vec3 diffuse_, glm::vec3 specular_, LightType type_);
     virtual ~Light();
+protected:
+    Light(glm::vec3 diffuse_, glm::vec3 specular_, LightType type_);
 };
