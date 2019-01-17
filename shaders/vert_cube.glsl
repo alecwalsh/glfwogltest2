0f3bf670a3185750fcf8a64674f3411a06ac16f2
Alec Walsh <alecwalsh1996@gmail.com>
2019-01-17 00:53:02 -0500

    Rewrote rotationMatrix function to ensure I can license it under BSD

--- shaders/vert_cube.glsl
+++ shaders/vert_cube.glsl
@@ -25,18 +25,24 @@ uniform mat4 bone_matrices[MAX_BONES];
 uniform mat4 bone_transforms[MAX_BONES];
 
 
-// mat4 rotationMatrix(vec3 axis, float angle)
-// {
-//     axis = normalize(axis);
-//     float s = sin(angle);
-//     float c = cos(angle);
-//     float oc = 1.0 - c;
-//     
-//     return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
-//                 oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
-//                 oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
-//                 0.0,                                0.0,                                0.0,                                1.0);
-// }
+mat4 rotationMatrix(vec3 axis, float theta) {
+	vec3 a = normalize(axis);
+	
+	float x = a.x;
+	float y = a.y;
+	float z = a.z;
+
+	float s = sin(theta);
+	float c = cos(theta);
+
+	return mat4(
+	            x * x * (1-c) + c,        x * y * (1-c) - z * s,   x * z * (1-c) + y * s, 0,
+	            x * y * (1-c) + z * s,    y * y * (1-c) + c,       y * z * (1-c) - x * s, 0,
+				x * z * (1-c) - y * s,    y * z * (1-c) + x * s,   z * z * (1-c) + c,     0,
+				0,                        0,                       0,                     1
+	);
+
+}
 
 void main() {
     Color = color;
