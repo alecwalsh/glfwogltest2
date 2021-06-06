38078da6d6da561e4ce5ff01807aa855565e3a4a
Alec Walsh <alecwalsh1996@gmail.com>
2021-06-06 19:08:38 -0400

    Remove unused vertex attributes from shaders

--- shaders/frag_light.glsl
+++ shaders/frag_light.glsl
@@ -2,11 +2,6 @@
 precision highp float;
 #endif
 
-in vec3 Color;
-in vec3 Normal;
-in vec3 FragPos;
-in vec2 Texcoord;
-
 layout(location = 0) out vec4 outColor;
 
 void main() {
--- shaders/vert_light.glsl
+++ shaders/vert_light.glsl
@@ -2,28 +2,12 @@
 precision highp float;
 #endif
 
-//TODO: remove unnecessary attribs after moving lightsObject into separate class
 layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 normal;
-layout(location = 2) in vec2 texcoord;
-layout(location = 3) in vec3 color;
-
-out vec3 Color;
-out vec3 Normal;
-out vec3 FragPos;
-out vec2 Texcoord;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 proj;
 
 void main() {
-    Color = color;
-
-    //Normal matrix
-    Normal = mat3(transpose(inverse(model))) * normal; //This is inefficient, should calculate on CPU
-
-    FragPos = vec3(1.0);
-    Texcoord = texcoord;
     gl_Position = proj * view * model * vec4(position, 1.0);
 }
